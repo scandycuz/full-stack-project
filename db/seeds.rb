b75d58b59492bf7e23b8b499b23418c1f2aa0ca7
scandycuz <trevorscandalios@gmail.com>
2016-11-02 11:47:09 -0700

    Seeds database with guest user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+users = User.create([{
+  first_name: 'Guest', last_name: 'User', email: 'guestuser@email.com', password: 'password'
+}])
