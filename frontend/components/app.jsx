01aefe16a3698f2b43850584fa21c8b7c2fbfe95
scandycuz <trevorscandalios@gmail.com>
2016-12-06 14:57:05 -0800

    Attempts to re add loading animation

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -19,6 +19,17 @@ class App extends React.Component {
         return (
           <div id="loading-screen">
             <div className="loader-container">
+              <div className="loader">
+                <div className="square" ></div>
+                <div className="square"></div>
+                <div className="square last"></div>
+                <div className="square clear"></div>
+                <div className="square"></div>
+                <div className="square last"></div>
+                <div className="square clear"></div>
+                <div className="square "></div>
+                <div className="square last"></div>
+              </div>
             </div>
           </div>
         )
