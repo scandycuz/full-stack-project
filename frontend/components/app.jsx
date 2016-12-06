514a04215de2f16f338a1f7b06f8a04074cd2e23
scandycuz <trevorscandalios@gmail.com>
2016-12-06 12:28:02 -0800

    Adds blank loading screen

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,45 +5,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      loading: true,
-      loadCount: 1
-    }
   }
 
   componentWillReceiveProps(nextProps) {
-    if (!nextProps.loading) {
-      this.setState({
-        loadCount: this.state.loadCount + 1
-      }, () => {
-        if (this.state.loadCount >= 2) {
-          this.setState({
-            loading: false,
-            loadCount: 0
-          })
-        }
-      });
-    }
+
   }
 
   render() {
     const loader = () => {
-      if (this.state.loading) {
+      if (this.props.loading) {
         return (
           <div className="loading-screen">
-            <div className="loader-container">
-              <div className="loader">
-                <div className="square" ></div>
-                <div className="square"></div>
-                <div className="square last"></div>
-                <div className="square clear"></div>
-                <div className="square"></div>
-                <div className="square last"></div>
-                <div className="square clear"></div>
-                <div className="square "></div>
-                <div className="square last"></div>
-              </div>
-            </div>
+            // <div className="loader-container">
+            //   <div className="loader">
+            //     <div className="square" ></div>
+            //     <div className="square"></div>
+            //     <div className="square last"></div>
+            //     <div className="square clear"></div>
+            //     <div className="square"></div>
+            //     <div className="square last"></div>
+            //     <div className="square clear"></div>
+            //     <div className="square "></div>
+            //     <div className="square last"></div>
+            //   </div>
+            // </div>
           </div>
         )
       }
