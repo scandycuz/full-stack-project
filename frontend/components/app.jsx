ffbebc576b4bf07367e4d3a84504143ffcb80e50
scandycuz <trevorscandalios@gmail.com>
2016-12-06 14:58:53 -0800

    Removes loading animation

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -19,17 +19,7 @@ class App extends React.Component {
         return (
           <div id="loading-screen">
             <div className="loader-container">
-              <div className="loader">
-                <div className="square" ></div>
-                <div className="square"></div>
-                <div className="square last"></div>
-                <div className="square clear"></div>
-                <div className="square"></div>
-                <div className="square last"></div>
-                <div className="square clear"></div>
-                <div className="square "></div>
-                <div className="square last"></div>
-              </div>
+              
             </div>
           </div>
         )
