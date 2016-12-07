ac8f936d3087d3dbf4e70320602fb7f0c5cfab8d
scandycuz <trevorscandalios@gmail.com>
2016-12-06 21:03:21 -0800

    Profile loading screen

--- frontend/components/campaign_show/campaign_show.jsx
+++ frontend/components/campaign_show/campaign_show.jsx
@@ -209,6 +209,17 @@ class CampaignShow extends React.Component {
         return (
           <div id="loading-screen">
             <div className="loader-container">
+              <div className="loader">
+                <div className="square" ></div>
+                <div className="square"></div>
+                <div className="square last"></div>
+                <div className="square clear"></div>
+                <div className="square"></div>
+                <div className="square last"></div>
+                <div className="square clear"></div>
+                <div className="square "></div>
+                <div className="square last"></div>
+              </div>
             </div>
           </div>
         )
