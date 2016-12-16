eeada7dc90f8bc41ef7a4213f1e0484738139cf0
scandycuz <trevorscandalios@gmail.com>
2016-12-15 23:00:10 -0800

    Fixes navigating from edit to campaign show loading bug

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -49,7 +49,7 @@ const Root = ({ store }) => {
             <Route path="/profile/:id/edit" component={ProfileEditContainer}/>
           </Route>
           <Route path="/campaigns/:id" component={CampaignContainer} onEnter={requestCampaign}>
-            <IndexRoute component={CampaignShowContainer}/>
+            <IndexRoute component={CampaignShowContainer} onEnter={requestCampaign}/>
             <Route path="/campaigns/:id/backers" component={CampaignShowContainer}/>
             <Route path="/campaigns/:id/edit" component={CampaignEditContainer}>
               <IndexRedirect to="/campaigns/:id/edit/basics" />
