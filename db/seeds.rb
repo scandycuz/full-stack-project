3ccff0a267cacde10d71c390e157af697f1c0ba4
scandycuz <trevorscandalios@gmail.com>
2017-01-09 23:19:27 -0800

    Updates campaign durations

--- db/seeds.rb
+++ db/seeds.rb
@@ -111,7 +111,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Mustache freegan gochujang venmo, actually lo-fi put a bird on it. 8-bit mlkshk austin, tofu chambray tilde kickstarter artisan messenger bag. +1 four dollar toast tumeric, offal bitters microdosing migas chicharrones. Fanny pack scenester tattooed before they sold out shabby chic. Quinoa chambray salvia before they sold out edison bulb.",
     status: "published",
     location: "San Francisco, CA, United States",
-    duration: "2017-01-04"
+    duration: "2017-02-04"
   },{ user_id: 1,
     title: "Tindr for Farmers",
     goal_amount: 2000,
@@ -123,7 +123,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Gluten-free kogi plaid kitsch narwhal. Single-origin coffee pabst sriracha heirloom, aesthetic authentic literally ramps bushwick affogato tilde hashtag franzen XOXO mixtape. Jean shorts authentic next level, glossier put a bird on it direct trade chia.",
     status: "published",
     location: "San Francisco, CA, United States",
-    duration: "2017-01-30"
+    duration: "2017-02-30"
   },{ user_id: 2,
     title: "Flikar",
     goal_amount: 2000,
@@ -135,7 +135,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Bushwick YOLO stumptown mlkshk, master cleanse tilde flexitarian readymade vexillologist pabst knausgaard slow-carb celiac leggings. La croix copper mug post-ironic cliche tacos normcore. Art party heirloom salvia DIY succulents 90's, microdosing vinyl umami dreamcatcher. Disrupt aesthetic master cleanse live-edge hexagon normcore, tattooed chartreuse butcher plaid fam lumbersexual 90's.",
     status: "published",
     location: "Portland, CA, United States",
-    duration: "2016-12-31"
+    duration: "2017-01-31"
   },{ user_id: 3,
     title: "Klerpit",
     goal_amount: 1000,
@@ -147,7 +147,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Tumblr mustache keffiyeh vegan, kickstarter squid vinyl. Pop-up la croix fap fanny pack. Unicorn truffaut godard portland 3 wolf moon blog. Cornhole +1 pabst, neutra 8-bit sustainable wayfarers tbh tacos street art shoreditch meggings. Four dollar toast typewriter williamsburg, intelligentsia quinoa deep v food truck flannel keytar. Meditation live-edge poutine, cred next level lumbersexual bespoke helvetica farm-to-table. Ethical keytar everyday carry, pabst ennui cliche master cleanse keffiyeh vegan celiac raclette.",
     status: "published",
     location: "San Francisco, CA, United States",
-    duration: "2017-01-28"
+    duration: "2017-02-28"
   },{ user_id: 4,
     title: "iBanjo",
     goal_amount: 4000,
@@ -159,7 +159,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Chia hell of street art small batch echo park edison bulb. Truffaut pinterest flexitarian, snackwave shoreditch mlkshk tbh fixie tilde. Pinterest selfies biodiesel meh 8-bit hot chicken. VHS tilde put a bird on it, asymmetrical meggings keytar actually irony chia small batch normcore farm-to-table. Wayfarers pitchfork vegan, gastropub twee XOXO asymmetrical mustache.",
     status: "published",
     location: "New York, NY, United States",
-    duration: "2016-12-24"
+    duration: "2017-01-24"
   },{ user_id: 5,
     title: "Vuzoo",
     goal_amount: 40000,
@@ -171,7 +171,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Chia hell of street art small batch echo park edison bulb. Truffaut pinterest flexitarian, snackwave shoreditch mlkshk tbh fixie tilde. Pinterest selfies biodiesel meh 8-bit hot chicken. VHS tilde put a bird on it, asymmetrical meggings keytar actually irony chia small batch normcore farm-to-table. Wayfarers pitchfork vegan, gastropub twee XOXO asymmetrical mustache.",
     status: "published",
     location: "Portland, OR, United States",
-    duration: "2017-01-08"
+    duration: "2017-02-08"
   },{ user_id: 6,
     title: "Listable",
     goal_amount: 8000,
@@ -183,7 +183,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Chia hell of street art small batch echo park edison bulb. Truffaut pinterest flexitarian, snackwave shoreditch mlkshk tbh fixie tilde. Pinterest selfies biodiesel meh 8-bit hot chicken. VHS tilde put a bird on it, asymmetrical meggings keytar actually irony chia small batch normcore farm-to-table. Wayfarers pitchfork vegan, gastropub twee XOXO asymmetrical mustache.",
     status: "published",
     location: "San Francisco, CA, United States",
-    duration: "2016-12-28"
+    duration: "2017-01-28"
   },{ user_id: 7,
     title: "jQuirky",
     goal_amount: 2000,
@@ -222,7 +222,7 @@ campaigns = Campaign.create([
     campaign_pitch: "Banjo drinking vinegar 8-bit neutra, vape hexagon mixtape bitters fingerstache edison bulb. Direct trade master cleanse dreamcatcher, vinyl green juice selvage austin squid taxidermy. Pour-over lomo slow-carb iPhone actually thundercats food truck, hammock banh mi ramps disrupt williamsburg waistcoat. Bespoke typewriter wolf, vegan hella banh mi keffiyeh +1 slow-carb. Authentic cronut williamsburg, asymmetrical you probably haven't heard of them pabst direct trade post-ironic kitsch waistcoat blue bottle microdosing.",
     status: "published",
     location: "San Francisco, CA, United States",
-    duration: "2017-01-30",
+    duration: "2017-02-12",
     featured: true
   },{ user_id: 6,
     title: "InspHired",
